188322134b1bcf4dd70fe4251d1a696150c1748c
Milan <milanpavlovic02@icloud.com>
2025-04-09 14:34:43 +0200

    feat: enhance contact form with email field and validation

--- components/contact-me/_components/form-contact.tsx
+++ components/contact-me/_components/form-contact.tsx
@@ -4,7 +4,6 @@ import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
@@ -19,6 +18,7 @@ const formSchema = z.object({
   username: z.string().min(2, {
     message: "Username must be at least 2 characters.",
   }),
+  email: z.string().email().min(1),
 });
 
 export function ContactForm() {
@@ -26,6 +26,7 @@ export function ContactForm() {
     resolver: zodResolver(formSchema),
     defaultValues: {
       username: "",
+      email: "",
     },
   });
 
@@ -42,19 +43,39 @@ export function ContactForm() {
           name="username"
           render={({ field }) => (
             <FormItem className="relative">
-              <FormLabel className="bg-background text-foreground/70 absolute start-2.5 z-10 -translate-y-1.5 scale-90">
+              <FormControl>
+                <Input {...field} className="peer/name is-dirty" />
+              </FormControl>
+              {/* TODO: FIX LABEL  */}
+              <FormLabel
+                htmlFor="username"
+                className="bg-background text-foreground/70 absolute start-2.5 top-4.5 -z-10 -translate-y-1.5 px-1 transition-all duration-300 peer-focus/name:top-0 peer-focus/name:z-10 peer-focus/name:scale-75 peer-[.is-dirty]/name:peer-focus/name:top-0"
+              >
                 Full Name
               </FormLabel>
-              <FormControl>
-                <Input {...field} className="peer pt-1.5" />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
+
               <FormMessage />
             </FormItem>
           )}
         />
+        {/* <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem className="relative">
+              <FormControl>
+                <Input {...field} className="peer" />
+              </FormControl>
+              <FormLabel
+                htmlFor="email"
+                className="bg-background text-foreground/70 absolute start-2.5 top-4.5 -z-10 -translate-y-1.5 transition-all duration-300 peer-focus:top-0 peer-focus:z-10 peer-focus:scale-75"
+              >
+                Email address
+              </FormLabel>
+              <FormMessage />
+            </FormItem>
+          )}
+        /> */}
         <Button type="submit">Submit</Button>
       </form>
     </Form>
