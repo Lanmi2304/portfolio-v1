677e041d8b24d04f536e2ed39ff5a8be215e87ff
Milan <milanpavlovic02@icloud.com>
2025-04-10 15:17:10 +0200

    feat(textarea): update padding from text-sm to text-base for improved readability

--- components/ui/textarea.tsx
+++ components/ui/textarea.tsx
@@ -9,7 +9,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
     return (
       <textarea
         className={cn(
-          "border-input bg-background ring-offset-background placeholder:text-muted-foreground focus-visible:ring-ring flex max-h-40 min-h-1 w-full resize-none rounded-md border p-2 text-sm focus-visible:ring-1 focus-visible:ring-offset-1 focus-visible:outline-hidden disabled:cursor-not-allowed disabled:opacity-50",
+          "border-input bg-background ring-offset-background placeholder:text-muted-foreground focus-visible:ring-ring flex max-h-40 min-h-1 w-full resize-none rounded-md border p-2 text-base focus-visible:ring-1 focus-visible:ring-offset-1 focus-visible:outline-hidden disabled:cursor-not-allowed disabled:opacity-50",
           className,
         )}
         ref={ref}
