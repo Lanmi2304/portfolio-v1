68f9a094acb41e5edd743d165cebf1659c346cb4
Milan <milanpavlovic02@icloud.com>
2025-05-16 21:13:29 +0200

    fix(get-base-url): add missing braces for development environment check

--- lib/utils/get-base-url.ts
+++ lib/utils/get-base-url.ts
@@ -1,5 +1,8 @@
 export const getBaseUrl = () => {
-  if (process.env.NODE_ENV === "development")
+  if (process.env.NODE_ENV === "development") {
     return process.env.NEXT_PUBLIC_APP_URL;
-  return process.env.NEXT_PUBLIC_VERCEL_URL;
+  }
+
+  const vercelUrl = process.env.NEXT_PUBLIC_VERCEL_URL;
+  return vercelUrl?.startsWith("http") ? vercelUrl : `https://${vercelUrl}`;
 };
