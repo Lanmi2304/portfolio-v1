c278755bfc8c30345371dcb8a8578ee260ed1cf6
Milan <milanpavlovic02@icloud.com>
2025-08-12 13:46:57 +0200

    feat(projects): add new project card component and update project data with images

--- components/projects/index.tsx
+++ components/projects/index.tsx
@@ -1,10 +1,10 @@
 "use client";
 import { PROJECTS } from "@/public/data/projects";
-import { ProjectCard } from "./_components/card-project";
-import { useState } from "react";
+import { ProjectCard1 } from "./_components/card-project-1";
+// import { useState } from "react";
 
 export function Projects() {
-  const [active, setActive] = useState<undefined | string>(undefined);
+  // const [active, setActive] = useState<undefined | string>(undefined);
   return (
     <div className="border-muted grid gap-10 border-t p-4 pt-10 md:p-10">
       <h3 className="bg-opacity-50 bg-gradient-to-b from-neutral-50 to-neutral-400 bg-clip-text text-4xl font-bold text-transparent md:text-4xl">
@@ -18,8 +18,9 @@ export function Projects() {
 
       <div className="grid grid-rows-1 gap-4 md:grid-cols-2">
         {PROJECTS.map((project) => (
-          <div key={project.title} onClick={() => setActive(project.title)}>
-            <ProjectCard active={active} project={project} />
+          // <div key={project.title} onClick={() => setActive(project.title)}>
+          <div key={project.title}>
+            <ProjectCard1 project={project} />
           </div>
         ))}
       </div>
